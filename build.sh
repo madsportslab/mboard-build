5821a0547311ee146855202cc6968e61bff9e1ca
stephenhu <epynonymous@outlook.com>
2018-02-18 23:05:47 -0800

    add database migrations

--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@ dir4="$HOME/build/mboard/DEBIAN"
 dir5="$HOME/build/mboard/home/mboard/.config/autostart"
 dir6="$HOME/build/mboard/home/mboard/bin/mboard-www"
 dir7="$HOME/build/mboard/home/mboard/bin/electron/resources"
+dir8="$HOME/build/mboard/home/mboard/data/migrations"
 
 log()
 {
@@ -30,6 +31,7 @@ init_dir()
   build_dir $dir4
   build_dir $dir5
   build_dir $dir6
+  build_dir $dir8
 }
 
 build_dir()
@@ -126,6 +128,19 @@ build_mboard_www()
   fi
 }
 
+build_migrations()
+{
+  if [ -d $src1/db/migrations ]; then
+
+    cd $src1/db/migrations
+    cp *.sql $dir8
+    log "Copied database migrations to $dir8"
+
+  else
+    lerror "Database migrations not found."
+  fi
+}
+
 build_systemctl()
 {
   cd $src4
@@ -183,6 +198,7 @@ build_mboard
 build_electron
 build_mboard_electron
 build_mboard_www
+build_migrations
 build_migrate
 build_systemctl
 build_autostart
